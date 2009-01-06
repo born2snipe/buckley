e85bdf3bef5b55980a347347f53c0a609be2d90e
Dan Dudley <born2snipe@gmail.com>
2009-01-06 17:57:42 -0600

    initial

---
+++ src/main/java/org/codeclub/buckley/App.java
@@ -0,0 +1,13 @@
+package org.codeclub.buckley;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
