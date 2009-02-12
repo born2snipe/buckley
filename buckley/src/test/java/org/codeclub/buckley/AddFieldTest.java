bfe550b5ff7922dfe9558a87474e228784a3f7b6
Dan Dudley <born2snipe@gmail.com>
2009-02-11 18:18:38 -0600

    updated readme

--- buckley/src/test/java/org/codeclub/buckley/AddFieldTest.java
+++ buckley/src/test/java/org/codeclub/buckley/AddFieldTest.java
@@ -1,8 +1,6 @@
 package org.codeclub.buckley;
 
-import com.lowagie.text.pdf.AcroFields;
-import com.lowagie.text.pdf.PdfReader;
-import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.*;
 import junit.framework.TestCase;
 import org.codeclub.buckley.compile.Compiler;
 import org.codeclub.buckley.compile.FontRegistry;
@@ -11,23 +9,78 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 
 
 public class AddFieldTest extends TestCase {
 
     public void test_() throws IOException, URISyntaxException {
-        File dir = new File("/Users/dan/Desktop");
-        File file = new File(dir, "send.pdf");
-        File compiled = new File(dir, "send_compiled.pdf");
+        File dir = new File("/Users/dan/Desktop/pdfs/");
+        File file = new File(dir, "ApplicationForPool2008-2009.pdf");
+        File compiled = new File(dir, "compiled.pdf");
 
         Compiler compiler = new org.codeclub.buckley.compile.Compiler(new FontRegistry());
 
-        File compiledPdf = compiler.compile(file, compiled, createTextFieldDocument("blah"));
+        Document document = new Document();
+
+        Page page = new Page(2);
+        page.addField(new TextField("blah", 106, 131, 240, 10));
+
+        document.addPage(page);
+
+        File compiledPdf = compiler.compile(file, compiled, document);
 
         PdfReader reader = new PdfReader(new FileInputStream(compiledPdf));
         AcroFields fields = reader.getAcroFields();
         assertNotNull(fields.getField("blah"));
     }
+
+    public void test_x() throws IOException {
+        File dir = new File("/Users/dan/Desktop/pdfs/");
+        File file = new File(dir, "ApplicationForPool2008-2009.pdf");
+
+        PdfReader reader = new PdfReader(new FileInputStream(file));
+
+        for (int i = 1; i < 1000; i++) {
+            PdfObject object = reader.getPdfObject(i);
+            if (object instanceof PdfDictionary && object.toString().equals("Dictionary of type: /Font")) {
+                PdfDictionary dictionary = (PdfDictionary) object;
+                for (Object key : dictionary.getKeys()) {
+                    System.out.println(key + " -- " + dictionary.get((PdfName) key));
+                }
+                System.out.println("*****************");
+            }
+        }
+    }
+
+    public void test_Y() throws IOException {
+        File dir = new File("/Users/dan/Desktop/pdfs/");
+        File file = new File(dir, "ApplicationForPool2008-2009.pdf");
+
+        PdfReader reader = new PdfReader(new FileInputStream(file));
+
+        AcroFields fields = reader.getAcroFields();
+        HashMap z = fields.getFields();
+        for (Object y : z.keySet()) {
+            System.out.println("y = " + y);
+            for (float i : fields.getFieldPositions((String) y)) System.out.println("i = " + i);
+
+            AcroFields.Item item = (AcroFields.Item) z.get(y);
+
+            for (Object value : item.values) {
+                System.out.println("value = " + value);
+                if (value instanceof PdfDictionary) {
+                    PdfDictionary dict = (PdfDictionary) value;
+                    for (Object o : dict.getKeys()) {
+                        System.out.println(value+" >> "+o+" -- "+dict.get((PdfName) o));
+                    }
+                }
+            }
+            System.out.println("========================");
+        }
+
+    }
+
 
     private Document createTextFieldDocument(String fieldName) {
         Page page = new Page(1);
